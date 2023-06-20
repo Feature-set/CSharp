c78e39bcb7e29bb970c904abdb1e2a5e4bbd8bc3
kory199 <97496901+kory199@users.noreply.github.com>
2023-06-20 14:35:00 +0900

    이진탐색재귀
    BSearchRecur

--- DataStructure/BinarySearch/BinarySearch/Program.cs
+++ DataStructure/BinarySearch/BinarySearch/Program.cs
@@ -10,6 +10,28 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int first = 0;
+            int last = array.Length - 1;
+            int target = 3;
+            int answer = 0;
+
+            answer = BSearchRecur(array, first, last, target);
+            Console.WriteLine(answer);
+        }
+
+        static public int BSearchRecur(int[] ar, int first, int last, int target)
+        {
+            int mid = (first + last) / 2;
+            if (first > last)
+                return -1; // 목표값이 배열에 없을 경우 -1을 반환
+
+            if (ar[mid] == target)
+                return mid;
+            else if (target < ar[mid])
+                return BSearchRecur(ar, first, mid - 1, target);
+            else
+                return BSearchRecur(ar, mid + 1, last, target);
         }
     }
 }
