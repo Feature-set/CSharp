38111d6e5f3f423b987b91f4db0838d4d1d46833
ShineSungHwan <97496901+kory199@users.noreply.github.com>
2023-09-08 16:35:45 +0900

    update

---
+++ FSM/FSM/FSM/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM
+{
+    internal enum State
+    {
+        Idle,
+        Attack,
+        Move,
+        Death,
+        Pause,
+        Max
+    }
+}
---
+++ FSM/FSM/FSM/StateMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM
+{
+    internal class StateMachine
+    {
+        private State curState;
+        
+        public StateMachine()
+        {
+            // 초기값 선언
+            curState = State.Idle;
+        }
+
+        public void TransState(State inputState)
+        {
+            // 상태 변경
+            curState = inputState;
+        }
+    }
+}
